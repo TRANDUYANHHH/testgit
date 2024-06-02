a0cfa7bab502513d019124ebe8d4bc988faa2767
DuyAnh <duyanhbs2019@gmail.com>
2024-06-03 00:17:33 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+alert("Welcome to my web");
+let s = prompt("Enter your name: ");
+alert("Hi " + s + ", have good time with JavaScript");
+let a = prompt("enter a: ");
+let b = prompt("enter b:");
+let c = a * b;
+prompt("a * b = ", c);
